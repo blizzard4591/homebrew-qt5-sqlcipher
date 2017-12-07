0ba43289308687ae4711c511ea6d682596256d9f
PBerger <philipp.berger@rwth-aachen.de>
2017-12-08 00:58:59 +0100

    Initial formula.

---
+++ Formula/qt5-sqlcipher.rb
@@ -0,0 +1,28 @@
+class Qt5Sqlcipher < Formula
+  desc "A Qt5 SQL driver plugin for SQLCipher."
+  homepage "https://github.com/blizzard4591/qt5-sqlcipher"
+  url "https://github.com/blizzard4591/qt5-sqlcipher/archive/1.0.0.zip"
+  version "1.0.0"
+  sha256 "6788abb6d49742ea49ec387e0177b2101dce16f4a42bf2292ac249e4c2459297"
+  head "https://github.com/blizzard4591/qt5-sqlcipher.git", :using => :git, :shallow => false
+
+  depends_on :macos => :mavericks
+  depends_on "cmake"
+  depends_on "qt5"
+  depends_on "sqlcipher"
+
+  def install
+    args = %w[
+      -DCMAKE_BUILD_TYPE=RELEASE
+    ]
+
+    mktemp do
+      system "cmake", buildpath, *(std_cmake_args + args)
+      system "make", "install"
+    end
+  end
+
+  test do
+    shell_output("#{bin}/qsqlcipher-test", 1)
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homebrew-qt5-sqlcipher
 A homebrew tap/formula for Qt5-Sqlcipher, a Qt5 wrapper for SQLCipher.
+
+# Installing qt5-sqlcipher via homebrew
+
+This tap provides the formula to install qt5-sqlcipher via homebrew. All you need to do is
+
+```sh
+brew tap blizzard4591/homebrew-qt5-sqlcipher
+brew install qt5-sqlcipher
+``` 
