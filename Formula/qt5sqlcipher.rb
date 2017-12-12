649ce9555f6505447e280020d84c07718f32a48c
P. Berger <admin@philippberger.de>
2017-12-12 14:46:02 +0100

    Update qt5sqlcipher.rb

--- Formula/qt5sqlcipher.rb
+++ Formula/qt5sqlcipher.rb
@@ -9,7 +9,7 @@ class Qt5sqlcipher < Formula
   depends_on :macos => :mavericks
   depends_on "cmake"
   depends_on "qt5"
-  depends_on "blizzard4591/homebrew-qt5-sqlcipher/sqlcipher"
+  depends_on "blizzard4591/homebrew-qt5-sqlcipher/sqlcipherwithcolumn"
 
   def install
     args = %w[
