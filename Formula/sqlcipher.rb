5aa22737cfa5a7216ce0867bc7e2693881f63829
PBerger <berger@cs.rwth-aachen.de>
2020-02-16 17:23:45 +0100

    Updated sqlcipher.rb formula.

--- Formula/sqlcipher.rb
+++ Formula/sqlcipher.rb
@@ -1,32 +1,24 @@
 class Sqlcipher < Formula
   desc "SQLite extension providing 256-bit AES encryption"
-  homepage "http://sqlcipher.net"
-  url "https://github.com/sqlcipher/sqlcipher/archive/v3.4.1.tar.gz"
-  sha256 "4172cc6e5a79d36e178d36bd5cc467a938e08368952659bcd95eccbaf0fa4ad4"
-  revision 3
-
+  homepage "https://www.zetetic.net/sqlcipher/"
+  url "https://github.com/sqlcipher/sqlcipher/archive/v4.3.0.tar.gz"
+  sha256 "fccb37e440ada898902b294d02cde7af9e8706b185d77ed9f6f4d5b18b4c305f"
   head "https://github.com/sqlcipher/sqlcipher.git"
 
-  depends_on "openssl"
-
-  # Upstream SQLite patch for CVE-2017-10989
-  patch :p0 do
-    url "https://sqlite.org/src/vpatch?from=0db20efe201736b3&to=66de6f4a9504ec26"
-    sha256 "41d0570cbf80429e556e612acd5eeddcff0e586264a6bcb80bd5a27abc9159a2"
-  end
+  depends_on "openssl@1.1"
 
   def install
     args = %W[
       --prefix=#{prefix}
       --enable-tempstore=yes
-      --with-crypto-lib=#{Formula["openssl"].opt_prefix}
+      --with-crypto-lib=#{Formula["openssl@1.1"].opt_prefix}
       --enable-load-extension
       --disable-tcl
     ]
 
-    # Hardcode required flags
+    # Build with full-text search enabled
     args << "CFLAGS=-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5 -DSQLITE_THREADSAFE=1 -DSQLITE_ENABLE_COLUMN_METADATA=1"
-    
+
     system "./configure", *args
     system "make"
     system "make", "install"
