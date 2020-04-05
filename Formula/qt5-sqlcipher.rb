1b73d596b203f4f73cf177de17552f2ca982f9b5
PBerger <berger@cs.rwth-aachen.de>
2020-04-05 16:54:19 +0200

    Fixed and updated formula.

--- Formula/qt5-sqlcipher.rb
+++ Formula/qt5-sqlcipher.rb
@@ -1,13 +1,12 @@
 class Qt5Sqlcipher < Formula
   desc "Qt5 SQL driver plugin for SQLCipher"
   homepage "https://github.com/blizzard4591/qt5-sqlcipher"
-  url "https://github.com/blizzard4591/qt5-sqlcipher/archive/v1.0.6.tar.gz"
-  sha256 "e0eae584eaa791158e203eda01627a6a2c93bb421eaf231b2b0552163297b26a"
+  url "https://github.com/blizzard4591/qt5-sqlcipher/archive/v1.0.10.tar.gz"
+  sha256 "e60206becbe37dc9edcb9bfdc98d794fe612de68818cb81cef95e95193cbd089"
   head "https://github.com/blizzard4591/qt5-sqlcipher.git", :using => :git, :shallow => false
 
-  depends_on "cmake"
-  depends_on :macos => :mavericks
-  depends_on "pkg-config"
+  depends_on "cmake" => [:build]
+  depends_on "pkg-config" => [:build]
   depends_on "qt"
   depends_on "sqlcipher"
 
@@ -24,6 +23,7 @@ class Qt5Sqlcipher < Formula
   end
 
   test do
-    shell_output("#{bin}/qsqlcipher-test", 1)
+    output = shell_output("#{bin}/qsqlcipher-test")
+    assert_match("Success! All tests completed.", output)
   end
 end
